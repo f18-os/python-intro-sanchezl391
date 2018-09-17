c436ed16b535d408c785d0b36a00ee4164ceacc1
luis sanchez <sanchezl391@gmail.com>
2018-09-17 15:34:07 -0700

    Pipe now works for some commands Ex: ls | cat

--- shell/main.py
+++ shell/main.py
@@ -1,36 +1,48 @@
 import os, re, sys
 
-# Get Parent Process Id, for printing purposes
-parentProcessId = os.getpid()
-
-# Parent Process Prompts: Writes to stdout
-os.write(1, "\nEnter a command: ".encode())
-
-# Create Shell Subprocess to handle execution of command
-processId = os.fork()
-if processId < 0:  
-    os.write(2, ("fork failed, returning %d\n" % processId).encode())
-    sys.exit(1)
-
-elif processId == 0:    # Child 
-    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
-                 (os.getpid(), parentProcessId)).encode())
-    # Read string from stdin
-    user_input = input().split()    
-
-
+def execute_cmd(cmd):
     for dir in re.split(":", os.environ['PATH']): # try each directory in path
-            # print(dir)
+        program = "%s/%s" % (dir, cmd[0])
+        try:
+            os.execve(program, cmd, os.environ) # try to exec command
+            os.close(sys.stdout)
+        except OSError:             # ...expected
+            pass  
+
+# Create SubProcess to emulate shell
+parentProcessId = os.fork() 
+
+if parentProcessId == 0: # Child Shell
+    os.write(1, "\nEnter a command: ".encode())
+    user_input = input().split() 
+
+    hasPipe = "|" in user_input
+    if(hasPipe):
+        pipeIndex = user_input.index('|') # Has Pipe
+        firstCmd = user_input[0 : pipeIndex]
+        secondCmd = user_input[pipeIndex + 1: len(user_input)]
+
+        r,w = os.pipe()
+        # Fork another process to connect pipe to       
+        childProcessId = os.fork()
+        
+
+        if(childProcessId == 0): # setting up child to read from pipe
+            os.dup2(r, sys.stdin.fileno())
+            os.close(r)
+            os.close(w)
+            execute_cmd(secondCmd)
+        else: # writing side of pipe
+            os.close(r)
+            os.dup2(w, sys.stdout.fileno())
+            execute_cmd(firstCmd)
+    else: # No Pipe
+        for dir in re.split(":", os.environ['PATH']): # try each directory in path
             program = "%s/%s" % (dir, user_input[0])
             try:
                 os.execve(program, user_input, os.environ) # try to exec command
-            except FileNotFoundError:             # ...expected
+                os.close(sys.stdout)
+            except OSError:             # ...expected
                 pass                              # ...fail quietly 
-    sys.exit(1)
-# Fork was successful, wait for child to terminate
-else: 
-    childPidCode = os.wait()
-    os.write(1, ("Child Shell Process Terminated with exit code %d\n\n" % childPidCode).encode())
-
-
-
+else: # Parent Shell
+    os.wait()
