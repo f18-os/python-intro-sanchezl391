1e4eea5be0b7d42e28c8ccf55e14e77c74918e21
Luis Sanchez <sanchezl391@gmail.com>
2018-09-18 22:11:11 -0700

    Used demo code to fix bugs. Piping still doesn't work

--- shell/main.py
+++ shell/main.py
@@ -7,12 +7,14 @@ def execute_cmd(cmd):
         program = "%s/%s" % (dir, cmd[0])
         try:
             os.execve(program, cmd, os.environ) # try to exec command
-            os.close(sys.stdout)
+            # os.close(sys.stdout)
         except OSError:             # ...expected
             pass  
 
 # Create SubProcess to emulate shell
 parentProcessId = os.fork() 
+
+import fileinput
 
 if parentProcessId == 0: # Child Shell
     os.write(1, "\nEnter a command: ".encode())
@@ -25,19 +27,34 @@ if parentProcessId == 0: # Child Shell
         secondCmd = user_input[pipeIndex + 1: len(user_input)]
 
         r,w = os.pipe()
+        for f in (r, w):
+            os.set_inheritable(f, True)
         # Fork another process to connect pipe to       
         childProcessId = os.fork()
-        
 
         if(childProcessId == 0): # setting up child to read from pipe
-            os.dup2(r, sys.stdin.fileno())
-            os.close(r)
-            os.close(w)
+            # os.dup2(r, 1)
+            # os.close(0)
+            # os.dup(r)
+            os.dup2(r, 0)
+            # for fd in (r, w):
+            #     os.close(fd)
+            readStr = ''
+            for line in fileinput.input():
+                readStr+=line
+            secondCmd.insert(0, readStr)
             execute_cmd(secondCmd)
         else: # writing side of pipe
-            os.close(r)
-            os.dup2(w, sys.stdout.fileno())
+            os.dup2(w, r)
+            # os.close(r)
+            # print("1")
+            # os.close(1)
+            # print("2")
+            # os.dup(w)
+            # for fd in (r, w):
+            #     os.close(fd)
             execute_cmd(firstCmd)
+            
     else: # No Pipe
         for dir in re.split(":", os.environ['PATH']): # try each directory in path
             program = "%s/%s" % (dir, user_input[0])
